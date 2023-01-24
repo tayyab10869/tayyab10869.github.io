5e871dc09226eee4f28119f9e0b56e7e5814ee80
tayyab10869 <113855512+tayyab10869@users.noreply.github.com>
2023-01-24 14:53:13 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,16 @@
+function openLetterPopup() {
+    document.getElementById("letterPopup").style.display = "block";
+  }
+  
+  function openPoetryPopup() {
+    document.getElementById("poetryPopup").style.display = "block";
+  }
+  
+  function closeLetterPopup() {
+    document.getElementById("letterPopup").style.display = "none";
+  }
+  
+  function closePoetryPopup() {
+    document.getElementById("poetryPopup").style.display = "none";
+  }
+  
